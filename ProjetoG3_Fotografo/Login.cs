a6888f36bf4e1f23aca06331aff71facd71896a5
Wesley Victor <vitorwesley1013m@gmail.com>
2023-10-24 10:30:52 -0300

    update

--- ProjetoG3_Fotografo/Login.cs
+++ ProjetoG3_Fotografo/Login.cs
@@ -16,5 +16,10 @@ namespace ProjetoG3_Fotografo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }
