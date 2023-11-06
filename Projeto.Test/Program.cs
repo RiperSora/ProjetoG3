8f1e55411ddb40917a7a293060585dd4f69db2b1
Wesley Victor <vitorwesley1013m@gmail.com>
2023-11-06 11:56:26 -0300

    update

--- Projeto.Test/Program.cs
+++ Projeto.Test/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            
+        }
+        catch (SqlException ex)
+        {
+            
+        }
+        catch (Exception ex)
+        {
+           
+        }
+        Console.ReadKey();
+    }
+}
