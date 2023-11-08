960841d439c39272bbd20c144c4168f5c265d5f2
RiperSora <matheus.need.for@hotmail.com>
2023-11-08 19:56:52 -0300

    update

---
+++ SQLQueryCalendario.sql
@@ -0,0 +1,9 @@
+﻿select *from Calendario
+drop table Calendario
+SELECT * FROM Calendario WHERE YEAR(DataCalendario) = 2024;
+
+CREATE TABLE Calendario (
+    IdEvento INT IDENTITY (1, 1) NOT NULL,
+    DataCalendario VARCHAR (50) NOT NULL,
+    Evento VARCHAR (200) NOT NULL
+);
