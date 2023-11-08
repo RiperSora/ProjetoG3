f99e2c0e157637f4141892dd68d7b9e0abf13edc
Wesley Victor <vitorwesley1013m@gmail.com>
2023-11-08 09:27:16 -0300

    update

---
+++ ProjetoG3.teste/AdmLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class AdmLogin
+{
+    static void Main(string[] args)
+    {
+        string nome = "Wesley";
+        string senha = "123";
+        ProjetoG3_Fotografo.DAL.AdmDAL usuario = new ProjetoG3_Fotografo.DAL.AdmDAL().EfetuarLogin(nome, senha);
+        if (usuario.Nome != null && usuario.Senha != null)
+        {
+            Console.WriteLine("Adm logado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("Adm não encontrado.");
+        }
+    }
+}
---
+++ ProjetoG3_Fotografo/DAL/AdmDAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoG3_Fotografo.DAL
+{
+    public class AdmDAL
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public AdmDAL EfetuarLogin(string nome, string senha)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=FAC0539641W10-1;Initial Catalog=ClickProducoesDB;User ID=sa;Password=123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select * from Administrador where NomeAdministrador='" + nome + "' AND Senha ='" + senha + "'", conn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            AdmDAL usuario = new AdmDAL();
+            if (dr.Read())
+            {
+                usuario.Id = Convert.ToInt32(dr["IdAdministrador"]);
+                usuario.Nome = (string)dr["NomeAdministrador"];
+                usuario.Senha = (string)dr["Senha"];
+            }
+            return usuario;
+        }
+    }
+}
