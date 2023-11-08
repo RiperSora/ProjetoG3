c359d586168307b33071f17498fff8bcd8e10ca0
gomesrfl <145561588+gomesrfl@users.noreply.github.com>
2023-11-08 11:08:39 -0300

    update

--- ProjetoG3_Fotografo/Cliente.cs
+++ ProjetoG3_Fotografo/Cliente.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,32 @@ namespace ProjetoG3_Fotografo
         private void BtnBuscarData_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Cliente_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=FAC0539641W10-1;Initial Catalog=ClickProducoesDB;User ID=sa;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from Cliente", conn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            BindingSource bs = new BindingSource();
+
+            bs.DataSource = dr;
+            gridEvento.DataSource = bs;
+
+            
+            if (dr.Read())
+            {
+                ColunaNome.HeaderText = dr["NomeCliente"].ToString();
+                ColunaSenha.HeaderText = dr["Senha"].ToString();
+                ColunaRG.HeaderText = dr["RG"].ToString();
+                ColunaCpf.HeaderText = dr["CPF"].ToString();
+                ColunaEndereco.HeaderText = dr["Endereco"].ToString();
+                ColunaTel.HeaderText = dr["Telefone"].ToString();
+                ColunaEmail.HeaderText = dr["Email"].ToString();
+            }
         }
     }
 }
