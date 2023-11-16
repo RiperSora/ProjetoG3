cf8ea5901e537d19acc6476c52b614d0561754cf
gomesrfl <145561588+gomesrfl@users.noreply.github.com>
2023-11-16 11:34:34 -0300

    update

--- ProjetoG3_Fotografo/Program.cs
+++ ProjetoG3_Fotografo/Program.cs
@@ -11,11 +11,9 @@ namespace ProjetoG3_Fotografo
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
+
             Application.Run(new Funcionario());
-=======
-            Application.Run(new Login());
->>>>>>> c4b5be9e4c5ab534ae467d048c7f2a26608cf387
+
         }
     }
 }
